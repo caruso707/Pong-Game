3a01f350deb8e6bca4b00eb8986f3250dcd71e34
caruso707@gmail.com <caruso707@gmail.com>
2023-03-06 17:40:51 -0500

    Updated to make consistent with snake game scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -3,13 +3,10 @@ from turtle import Turtle
 
 class Scoreboard(Turtle):
 
-    def __init__(self, pos_x=0, pos_y=0, color="yellow", clear_color="black", align="center",
-                 font=("Verdana", 30, "normal")):
+    def __init__(self, pos_x=0, pos_y=0, color="yellow", align="center", font=("Verdana", 30, "normal")):
         super().__init__()
         self.pos_x = pos_x
         self.pos_y = pos_y
-        self.clear_color = clear_color
-        self.write_color = color
         self.align = align
         self.font = font
         self.hideturtle()
@@ -25,3 +22,16 @@ class Scoreboard(Turtle):
         self.score += 1
         self.goto(self.pos_x, self.pos_y)
         self.write(f"{self.score}", font=self.font, align=self.align)
+
+    def update_score(self, new_score: int):
+        """Updates scoreboard to show new score"""
+        self.clear()
+        self.score = new_score
+        self.goto(self.pos_x, self.pos_y)
+        self.write(f"Score: {self.score}", font=self.font, align=self.align)
+
+    def game_over(self, text: str):
+        """Prints string, followed by final score."""
+        self.clear()
+        self.goto(self.pos_x, self.pos_y)
+        self.write(f"{text} Final Score: {self.score}", font=self.font, align=self.align)
